e873442bbd1709804b5daf492523501268465dae
CalenDario13 <56509446+CalenDario13@users.noreply.github.com>
2020-04-04 21:03:30 +0200

    Add files via upload
    Versione Ganza

---
+++ main.py
@@ -0,0 +1,57 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import utils
+import csv
+
+# Our values:
+
+nodes = range(10, 1050, 50)
+degree = 4
+lambd = 1.4
+
+# Generate a csv to save data
+
+PATH_R = '/Users/Dario/Google Drive/DS/First Year - Secon Semester/NBD/DC_Homeworks/HW_1/results.csv'
+results = open(PATH_R, 'w')
+write_r = csv.writer(results)
+write_r.writerow(['nodes', 'graph_type' ,'is_connected', 'bfs_time', 'adj_time', 'laplacian_time'])
+results.close()
+
+# Work on a regular graph:
+
+utils.check_connectivity("r-graph", nodes, degree, PATH_R)
+
+# Work on a er_graph:
+
+utils.check_connectivity("er-graph", nodes, degree, PATH_R, lambd)
+
+
+# Generate DFs:
+
+results_df = pd.read_csv(PATH_R)
+
+
+df_melt_er = pd.melt(results_df, id_vars = ['nodes', 'graph_type'], 
+        value_vars = ['bfs_time', 'adj_time', 'laplacian_time']).query("graph_type == 'er-graph'")
+
+df_melt_r = pd.melt(results_df, id_vars = ['nodes', 'graph_type'], 
+        value_vars = ['bfs_time', 'adj_time', 'laplacian_time']).query("graph_type == 'r-graph'")
+
+# Plot results:
+
+plt.figure(1)
+sns.lineplot(x = 'nodes', y = 'value', hue = 'variable', data = df_melt_r)
+plt.title("Regular-Graph Connectivity Checking", pad = 10)
+plt.ylabel("log(Time)")
+plt.yscale("log")
+plt.legend(title = '', labels=['BFS', 'Adjacency', 'Laplacian'])
+
+plt.figure(2)
+sns.lineplot(x = 'nodes', y = 'value', hue = 'variable', data = df_melt_er)
+plt.title("ErdosRenyi-Graph Connectivity Checking", pad = 10)
+plt.ylabel("log(Time)")
+plt.yscale("log")
+plt.legend(title = '', labels=['BFS', 'Adjacency', 'Laplacian'])
+plt.show()
+
